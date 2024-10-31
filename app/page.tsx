8c8ba09f9ae06a738b04fd30b9dc8023f136442d
Jabal Logian <jaballogianrma77@gmail.com>
2024-10-31 10:21:47 +0700

    add hero counters utility

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() { 
+'use client'
+import React, { useEffect } from 'react'
+import { getHeroCounters } from './utilities/heroCounters'
+
+const App: React.FC = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const result = await getHeroCounters()
+      console.log(result)
+    }
+
+    getData()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      Home
+    </div>
   )
 }
+
+export default App
